291183b6552104742d7421f2f4627b800b8773e6
Pierre-Alexis de Solminihac <pa@quai13.com>
2013-04-04 11:45:10 +0200

    CONTENUS_HTML 3.1     meilleure conformite avec core 3

--- ctrl/contenus_htmlContenusController.php
+++ ctrl/contenus_htmlContenusController.php
@@ -10,7 +10,6 @@
  */
 class contenus_htmlContenusController extends contenus_htmlContenusController_Parent
 {
-
     /**
      * valid_clementine_cms_contenu_htmlAction 
      * 
@@ -18,17 +17,17 @@ class contenus_htmlContenusController extends contenus_htmlContenusController_Pa
      * @access public
      * @return void
      */
-    function valid_clementine_cms_contenu_htmlAction($request) 
+    function valid_clementine_cms_contenu_htmlAction($request, $params = null) 
     {
+        $ns = $this->getModel('fonctions');
         if ($this->getModel('users')->needPrivilege('manage_contents')) {
-            $ns = $this->getModel('fonctions');
-            if (!empty($_POST)) {
+            if (!empty($request->POST)) {
                 $type_content = 'clementine_cms_contenu_html';
-                $id           = $ns->ifPost('int', 'id');
-                $id_zone      = $ns->ifPost('int', 'id_zone');
-                $id_page      = $ns->ifPost('int', 'id_page');
-                $nom          = $ns->ifPost('html', 'nom');
-                $contenu_html = $ns->ifPost('html', 'contenu_html');
+                $id           = $request->post('int', 'id');
+                $id_zone      = $request->post('int', 'id_zone');
+                $id_page      = $request->post('int', 'id_page');
+                $nom          = $request->post('html', 'nom');
+                $contenu_html = $request->post('html', 'contenu_html');
                 $contenus = $this->getModel('contenus');
                 // ajoute le contenu s'il n'existe pas deja
                 $request = $this->getRequest();
@@ -36,7 +35,7 @@ class contenus_htmlContenusController extends contenus_htmlContenusController_Pa
                 if (!$id) {
                     $id = $contenus->addContenu($nom, $type_content, $id_zone, $id_page, $lang);
                 }
-                if ($this->set_contenu_defaut($id)) {
+                if ($this->set_contenu_defaut($request, $id)) {
                     $contenus->updateContenuHtml($id, $contenu_html, $lang);
                 }
             }
@@ -46,7 +45,7 @@ class contenus_htmlContenusController extends contenus_htmlContenusController_Pa
                 $ns->redirect(__WWW__ . '/cms');
             }
         } else {
-            $this->getModel('fonctions')->redirect(__WWW__);
+            $ns->redirect(__WWW__);
         }
     }
 }
